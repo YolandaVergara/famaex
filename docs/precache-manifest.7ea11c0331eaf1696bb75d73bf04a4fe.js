faf95b7f3297f9ae22414e62a268502474af84cd
Yolanda <yv.avila@gmail.com>
2020-02-16 22:33:23 +0100

    wip

---
+++ docs/precache-manifest.7ea11c0331eaf1696bb75d73bf04a4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1019da703db5c738b1567f7830f55d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e6d5c1bed70fa79e1d8",
+    "url": "/static/css/main.11e25a78.chunk.css"
+  },
+  {
+    "revision": "b7731a86c5fcb5cc3a62",
+    "url": "/static/js/2.64a156ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.64a156ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6d5c1bed70fa79e1d8",
+    "url": "/static/js/main.72a7176c.chunk.js"
+  },
+  {
+    "revision": "1c4a57c1446b4cff1c99",
+    "url": "/static/js/runtime-main.d86b35da.js"
+  }
+]);
